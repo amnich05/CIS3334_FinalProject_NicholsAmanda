2810f311b6094e36d5b1b9f0e0ad021c1dcc41a1
Amanda Nichols <anichols1@css.edu>
2018-05-13 11:29:14 -0500

    Amanda Nichols -- Progress with functionality -- Added firebaseAuth to WelcomeActivity to check if user is already signed in, if so - directs user to MainActivity.

--- app/src/main/java/edu/css/amanda/cis3334_finalproject_nicholsamanda/WelcomeActivity.java
+++ app/src/main/java/edu/css/amanda/cis3334_finalproject_nicholsamanda/WelcomeActivity.java
@@ -1,21 +1,41 @@
 package edu.css.amanda.cis3334_finalproject_nicholsamanda;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     private Button buttonWelcomeLogin;
     private Button buttonWelcomeCreateAccount;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() { //initialized mAuthListener
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                //track the user when they sign in or out using the firebaseAuth
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("CSS3334","onAuthStateChanged - User IS signed in");
+                    Intent signInIntent = new Intent(getBaseContext(), MainActivity.class);
+                    startActivity(signInIntent);
+                }
+            }
+        };
 
         buttonWelcomeLogin = (Button) findViewById(R.id.buttonWelcomeLogin);
         buttonWelcomeLogin.setOnClickListener(new View.OnClickListener() {
